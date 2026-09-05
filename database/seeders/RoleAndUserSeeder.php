7d47c6f6aa980b2e58ca03ff9efa1b42f29d66c0
RifanRPL <rifanrpl01@gmail.com>
2026-09-05 10:26:40 +0700

    Tambahan Seed User Customer dan Petugas

--- database/seeders/RoleAndUserSeeder.php
+++ database/seeders/RoleAndUserSeeder.php
@@ -15,21 +15,55 @@ class RoleAndUserSeeder extends Seeder
     {
         // 1. Insert data ke tabel roles
         DB::table('roles')->insert([
-            'id' => 1,
-            'nama_role' => 'admin',
-            'created_at' => now(),
-            'updated_at' => now(),
+            [
+                'id' => 1,
+                'nama_role' => 'admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'nama_role' => 'petugas',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 3,
+                'nama_role' => 'customer',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
 
         // 2. Insert data ke tabel users
         DB::table('users')->insert([
-            'name' => 'Admin User',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('password123'),
-            'role_id' => 1,
-            'no_telp' => '081234567890',
-            'created_at' => now(),
-            'updated_at' => now(),
+            [
+                'name' => 'Admin User',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('admin123'),
+                'role_id' => 1,
+                'no_telp' => '081234567890',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Petugas User',
+                'email' => 'petugas@gmail.com',
+                'password' => Hash::make('petugas123'),
+                'role_id' => 2,
+                'no_telp' => '081234567891',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Customer User',
+                'email' => 'customer@gmail.com',
+                'password' => Hash::make('customer123'),
+                'role_id' => 3,
+                'no_telp' => '081234567892',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
     }
 }
